5830c1467308aeef17ad61aac889a133eaade26b
Rubén Zafra <rubeen722@gmail.com>
2024-10-17 23:34:13 +0200

    fixed deploy

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,26 +1,22 @@
-import { Pool, neonConfig } from "@neondatabase/serverless";
+import { Pool } from "@neondatabase/serverless";
 import { PrismaNeon } from "@prisma/adapter-neon";
 import { PrismaClient } from "@prisma/client";
-import ws from "ws";
-
-import "server-only";
 
 declare global {
-  // eslint-disable-next-line no-var, no-unused-vars
+  // eslint-disable-next-line no-var
   var cachedPrisma: PrismaClient;
 }
 
-const connectionString = `${process.env.DATABASE_URL}`;
-const pool = new Pool({ connectionString });
-const adapter = new PrismaNeon(pool);
-let prisma = new PrismaClient({ adapter });
-neonConfig.webSocketConstructor = ws;
-
+let prisma: PrismaClient;
 if (process.env.NODE_ENV === "production") {
-  prisma = new PrismaClient();
+  const pool = new Pool({ connectionString: process.env.DATABASE_URL });
+  const adapter = new PrismaNeon(pool);
+  prisma = new PrismaClient({ adapter });
 } else {
   if (!global.cachedPrisma) {
-    global.cachedPrisma = new PrismaClient();
+    const pool = new Pool({ connectionString: process.env.DATABASE_URL });
+    const adapter = new PrismaNeon(pool);
+    global.cachedPrisma = new PrismaClient({ adapter });
   }
   prisma = global.cachedPrisma;
 }
